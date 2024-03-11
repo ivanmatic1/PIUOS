f83c67856c96116858aad7d097d271f05cc36343
Matija <kiseljak.matija7@gmail.com>
2024-03-11 21:50:45 +0100

    registracija korisnik

--- account/serializer.py
+++ account/serializer.py
@@ -2,21 +2,21 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 
-class RegisterSerializer(serializers.Serializer):
+class RegisterSerializer(serializers.Serializer): #seriaizer za registraciju korisnika
     first_name = serializers.CharField()
     last_name = serializers.CharField()
     username  =  serializers.CharField()
     password = serializers.CharField()
 
 
-    def validate(self, data):
+    def validate(self, data): #funkcija za validiranje korisnika
         
-        if User.objects.filter(username = data['username']).exists():
+        if User.objects.filter(username = data['username']).exists(): #ukoliko korisnik upise username koji je već u bazi, javlja poruku
             raise serializers.ValidationError('Username is taken')
         
-        return data
+        return data #else vraća unesene podatke
     
-    def create(self, validated_data):
+    def create(self, validated_data): #funkcija za kreirane accounta
         user = User.objects.create(first_name = validated_data['first_name'],
             last_name =  validated_data['last_name'],
             username = validated_data['username'].lower()                
--- account/views.py
+++ account/views.py
@@ -5,23 +5,21 @@ from rest_framework import status
 from .serializer import RegisterSerializer
 
 
-class RegisterView(APIView):
+class RegisterView(APIView): #view za registraciju korisnika
     
-    def post(self, request):
+    def post(self, request): #funkcija za postanje podataka za registraciju
         data = request.data
         serializer =  RegisterSerializer(data=data)
         try:
-            
-            
 
-            if not serializer.is_valid():
+            if not serializer.is_valid(): #ako serializer nije validan
                 return Response({
                     'data':serializer.errors,
                     'message': 'Something went wrong'
                 }, status = status.HTTP_400_BAD_REQUEST)
             
             serializer.save()
-            return Response(
+            return Response( #else account se kreira
             {
                 'data':{},
                 'message': 'Your account is created succesfully!',
@@ -29,7 +27,7 @@ class RegisterView(APIView):
             )
             
 
-        except Exception as e:
+        except Exception as e:  #raisa exception ako nešto nije u redu
                if not serializer.is_valid():
                 return Response(
                 {
